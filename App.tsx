259d1b9b717daab526183cc23af0c2b2bdb25400
Minh Tran <trcongminh.261002@gmail.com>
2025-07-30 10:49:40 +0700

    Enhance App layout and update dependencies
    - Updated App.tsx to include global CSS and modified text styling using Tailwind CSS classes.
    - Added navigation dependencies: @react-navigation/bottom-tabs, @react-navigation/native, and @react-navigation/native-stack.
    - Included nativewind for utility-first CSS styling.
    - Updated react-native-reanimated, react-native-safe-area-context, and react-native-screens to their latest versions.
    - Added prettier-plugin-tailwindcss and tailwindcss for improved styling capabilities.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='text-4xl font-bold text-red-500'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
